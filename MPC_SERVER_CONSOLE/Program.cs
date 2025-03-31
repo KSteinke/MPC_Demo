b39b02ac6e7c727e010c237553f6f820b679866f
KSteinke <kacpersteinkesb@gmail.com>
2025-03-31 20:47:38 +0200

    MCP Server setted up on console app

--- MPC_SERVER_CONSOLE/Program.cs
+++ MPC_SERVER_CONSOLE/Program.cs
@@ -1,11 +1,25 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using System.Net.Http.Headers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using ModelContextProtocol;
 
 var builder = Host.CreateEmptyApplicationBuilder(settings: null);
 
-builder.Services.AddMcpServer()
-    .WithStdioServerTransport()
+builder.Services.AddMcpServer() //Create McpServer
+    .WithStdioServerTransport() 
     .WithToolsFromAssembly();
 
 
-    
+builder.Services.AddSingleton(_ =>
+{
+    var httpClient = new HttpClient()
+    {
+        BaseAddress = new Uri("https://api.weather.gov")
+    };
+    httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("weather-tool", "1.0"));
+    return httpClient;
+});
+
+var app = builder.Build();
+
+await app.RunAsync();
